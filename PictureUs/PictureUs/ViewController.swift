967e4507a1069247382a8c2cad3b0caaea628335
Charlie <charliewang92@gmail.com>
2016-09-25 19:53:49 -0700

    added a scroll view and added facebook and twitter share features. Requires actual phone to test

--- PictureUs/PictureUs/ViewController.swift
+++ PictureUs/PictureUs/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let v2 : View2 = View2(nibName: "cameraView", bundle: nil)
+        
+        self.addChildViewController(v2)
+        self.scrollView.addSubview(v2.view)
+        v2.didMoveToParentViewController(self)
+        
+        var v2Frame : CGRect = v2.view.frame
+        v2Frame.origin.x = self.view.frame.width
+        v2.view.frame = v2Frame
+        
+        self.scrollView.contentSize = CGSizeMake(self.view.frame.width * 3, self.view.frame.height)
+    }
+    @IBAction func twitterPush(sender: AnyObject) {
+        print("twitter pushed, will try and share")
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter){
+            let twitterSheet:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            twitterSheet.setInitialText("Share on Twitter")
+            self.presentViewController(twitterSheet, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Accounts", message: "Please login to a Twitter account to share.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func facebookPush(sender: AnyObject) {
+        print("facebook pushed, will try and share")
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook){
+            let facebookSheet:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            facebookSheet.setInitialText("Share on Facebook")
+            self.presentViewController(facebookSheet, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Accounts", message: "Please login to a Facebook account to share.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
