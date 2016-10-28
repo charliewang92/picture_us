cab738cd844bfce8b341ce54e2ca9bc89602c1b7
Charlie <charliewang92@gmail.com>
2016-10-28 16:00:57 -0700

    added AWS integration into app, next step is to add signin

--- PictureUs/PictureUs/AppDelegate.swift
+++ PictureUs/PictureUs/AppDelegate.swift
@@ -14,9 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return AWSMobileClient.sharedInstance.didFinishLaunching(application: application, withOptions: launchOptions)
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        return AWSMobileClient.sharedInstance.withApplication(application: application, withURL: url, withSourceApplication: sourceApplication, withAnnotation: annotation)
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        AWSMobileClient.sharedInstance.applicationDidBecomeActive(application: application)
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
@@ -33,9 +40,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
     
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
     
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
